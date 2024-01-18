bad3d368eaa7a92772b17755f351a828ff893e95
x37v <x37v@users.noreply.github.com>
2024-01-18 16:30:51 +0000

    Deploy code docs to GitHub Pages, Github Action id: $GITHUB_ACTION \nCommit: $GITHUB_SHA 433b3dc5e819d9c35d36fdad58f93003b487f331

---
+++ html/navtreedata.js
@@ -0,0 +1,89 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "OSSIA", "index.html", [
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_ossia_u_i_8h_source.html",
+"class_contains.html",
+"classopp_1_1value.html#ae2443aeef18be77cb9ae69625897aead",
+"classossia_1_1net_1_1device__base.html#a94906786ef040dd199151e6184cee4b9",
+"classossia_1_1net_1_1websocket__client.html#afac746b25c9b5101b21e7af145115673",
+"classossia_1_1time__sync.html#a78d95ff53389067086e33b0937c410e1",
+"execution__state_8hpp_source.html",
+"group___c_log.html#ga2a364bbba85201a033baa525cb6e3eea",
+"group___c_proto.html#ga37fa6608347f26863ee91d1d91c2a8c0",
+"minuit__common_8hpp.html#ab69f1b5e1f091f8964e68a16e1ca8584",
+"namespaceossia.html#a381fff66125b6a1ea4c919590cedcb0f",
+"namespaceossia.html#a92a788b8efe78f4ae1581c71d60abb3cac0cc02c3b3d55abb7bfb49ddbb4866c8",
+"namespaceossia.html#ae1230ee25c28cf56fe1bec34d0d31cdc",
+"node__attributes_8hpp.html#a44fceccd3e75cf0cceb276eb61c8083a",
+"osc__address_8cpp.html#a750ef70ac69dc27826ff5be5965b5137",
+"path_8cpp.html#a64a7dadd4c6c65b579d5373f7faed661",
+"structossia_1_1domain.html#a203ffd1db357f07ec507ab6ad0105257",
+"structossia_1_1regex__path_1_1any__path.html",
+"tinyspline_8h.html#a565ff3761d90530714283629d1850e9aac61a4b4715b7cf4f5f0fe23181c862b6"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
